33e4cacb09a1ff84d42f4294de0905a31b07efa7
SreekanthChinnakotla <134213780+SreekanthChinnakotla@users.noreply.github.com>
2025-01-23 17:54:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function displayTime() {
+  const now = new Date();
+ const options = { month: '2-digit', day: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit' };
+  const formattedTime = now.toLocaleString('en-US', options);
+
+  document.getElementById('timer').textContent = formattedTime;
+}
+
+// Update time every second
+setInterval(displayTime, 1000); 
+
+// Initial display
+displayTime();
